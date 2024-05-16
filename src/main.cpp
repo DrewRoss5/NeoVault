d1bd9ca9a5c1245a28441ea8b7b52fb2310b402f
DrewRoss5 <145530440+DrewRoss5@users.noreply.github.com>
2024-05-15 22:49:49 -0600

    Create a CLI demo of single file encryption

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,58 @@
 #include <iostream>
+#include <fstream>
 #include <sodium/randombytes.h>
 #include "cryptoutils.h"
 
 using namespace crypto;
 
 int main(int argc, char* argv[]){
-    if (argc != 2){
-        std::cout << "Please provide exactly one argument (a sample password) for this demo" << std::endl;
+    if (argc != 3){
+        std::cout << "This demo takes exactly two arugments." << std::endl;
     }
-    // create the cipherfile 
-    CipherFile cipher("../test/example1.txt", argv[1]);
-    // display the encrypted and decrypted versions of the file 
-    std::cout << "Exported Ciphertext: " << hex_string(cipher.export_ciphertext().get(), cipher.size()) << "\nDecrypted Plaintext: " << (char*) cipher.decrypt(argv[1]).get() << std::endl;
-    std::cout << "\nOther information:\n" << "\tNonce: " << hex_string(cipher.nonce(), NONCE_SIZE) << "\n\tSalt: " << hex_string(cipher.salt(), SALT_SIZE) << std::endl;
-
-    
-
-
+    // determine the command the user wants to run 
+    std::string command(argv[1]);
+    if (command == "encrypt_f"){
+        // get the password to encrypt the file with 
+        std::string password;
+        std::cout << "Password: ";
+        std::getline(std::cin, password);
+        // attempt to encrypt the provided file
+        try{
+            CipherFile ciphertext(argv[2], password);
+            // export the encrypted files
+            std::basic_ofstream<unsigned char> out_file(argv[2]);
+            ciphertext.write_to_file(out_file);
+            out_file.close();
+            std::cout << "Size: " << ciphertext.size() << "\nSalt: " << hex_string(ciphertext.salt(), SALT_SIZE) << "\nNonce: " << hex_string(ciphertext.nonce(), NONCE_SIZE) << "\nCiphertext: " << hex_string(ciphertext.content(), ciphertext.size()) << std::endl;
+            std::cout << "File Encrypted Succesfully" << std::endl;
+        }
+        catch (std::exception err){
+            std::cout << err.what() << std::endl;
+            return 1;
+        }
+    }
+    else if (command == "decrypt_f"){
+        // get the password to decrypt the file with 
+        std::string password;
+        std::cout << "Password: ";
+        std::getline(std::cin, password);
+        // attempt to decrypt the file 
+        try{
+            CipherFile ciphertext(argv[2]);
+            std::cout << "Size: " << ciphertext.size() << "\nSalt: " << hex_string(ciphertext.salt(), SALT_SIZE) << "\nNonce: " << hex_string(ciphertext.nonce(), NONCE_SIZE) << "\nCiphertext: " << hex_string(ciphertext.content(), ciphertext.size()) << std::endl;
+            std::unique_ptr<unsigned char[]> plaintext = ciphertext.decrypt(password);
+            // export the plaintext
+            std::basic_ofstream<unsigned char> out_file(argv[2]);
+            out_file.write(plaintext.get(), (ciphertext.size() - AES_OVERHEAD_SIZE));    
+            out_file.close();
+            std::cout << "File Decrypted Succesfully" << std::endl;
+        }
+        catch (std::exception err){
+            std::cout << err.what() << std::endl;
+            return 1;            
+        }
+    }
+    else{
+        std::cout << "Unrecognized Command: \"" << command << "\"" << std::endl;
+    }
 }
